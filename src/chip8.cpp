dfbd5ceda1110256f840c8be7655bc50c30c6e0b
Martina Fruttidoro <fruttidoro95@gmail.com>
2023-04-26 19:01:33 +0200

    I did the method to read a file and transform it into vector of instructions

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -1,3 +1,6 @@
 #include "chip8.h"
 
-int main() {}
+int main() {
+    Chip8 chip8;
+    chip8.readFromFile("/home/martina/cpp/chip8/programs/Breakout [Carmelo Cortez, 1979].ch8");
+}
--- src/chip8.h
+++ src/chip8.h
@@ -2,44 +2,74 @@
 
 #include <iostream>
 #include <array>
-#include  <cstddef>
-#include <bitset>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <cassert>
+#include <vector>
+
 
 class Register{
 public:
-    Register() : reg{std::bitset<8>()} {}
+    Register() : reg{ 0 } {}
 private:
-    std::bitset<8> reg; // the usual register has 8 bits
+    uint8_t reg; // the usual register has 8 bits
 };
 
 class Address{
 public:
-    Address() : address{ std::bitset<16>() } {}
+    Address() : address{ 0 } {}
 private:
-    std::bitset<16> address; // an address consists of 12 bits, so only the rightmost 12 bits are gonna be filled
+    uint16_t address; // an address consists of 12 bits, so only the rightmost 12 bits are gonna be filled
 };
 
 class Chip8{
 public:
     Chip8() :
-    ram{ std::array<std::bitset<8>, 4096>() },
+    ram{ std::array<uint8_t, 4096>() },
     registers{ std::array<Register, 16>() },
-    I{ std::bitset<16>() },
+    I{ 0 },
     timer{ Register() },
     sound{ Register() },
-    PC{ std::bitset<16>() },
-    stackPointer{ std::bitset<8>() },
+    PC{ 0 },
+    stackPointer{ 0 },
     stack{ std::array<Address, 16>() }  {}
+
+    std::vector<uint8_t> readFromFile(std::filesystem::path path)
+    {
+        assert(exists(path));
+        std::vector<uint8_t> output;
+        std::ifstream file {path};
+        if (file.is_open())
+        {
+            uint8_t byte;
+            while (!file.eof())
+            {
+                file >> byte;
+                output.push_back(byte);
+            }
+            for (uint8_t v : output)
+            {
+                std::cout << std::hex << (uint16_t) v << " ";
+            }
+            return output;
+        }
+        else
+        {
+            std::cout << "Unable to open file.\n";
+            return {};
+        }
+    }
 
 private:
     // the ram consists of register 0 to 4095 and each register has 8 bits
-    std::array<std::bitset<8>, 4096> ram;
+    std::array<uint8_t, 4096> ram;
 
     std::array<Register, 16> registers; // chip-8 has 16 registers of 8 bits
-    std::bitset<16> I; // 16-bits register to store memory address
+    uint16_t I; // 16-bits register to store memory address
     Register timer; // 8-bits register for delay
     Register sound; // 8-bits register for sound
-    std::bitset<16> PC; // program counter
-    std::bitset<8> stackPointer; // 8 bits for pointing to the topmost level of the stack
+    uint16_t PC; // program counter
+    uint8_t stackPointer; // 8 bits for pointing to the topmost level of the stack
     std::array<Address, 16> stack; // the stack is an array of 16 16-bits values to store addresses
 };
