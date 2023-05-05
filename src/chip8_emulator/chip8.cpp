9f038f791b3511f283012d1f7a47951ebb33f957
Martina Fruttidoro <fruttidoro95@gmail.com>
2023-05-05 19:47:28 +0200

    added classes display and pixel and implemented drw, still to test

--- src/chip8_emulator/chip8.cpp
+++ src/chip8_emulator/chip8.cpp
@@ -1,32 +1,68 @@
 #include "chip8.h"
+#include <cassert>
 
-std::string Register::toString() const
+std::string Chip8_internals::Register::toString() const
 {
     std::stringstream stream;
     stream << "0x" << std::hex << static_cast<uint16_t>(reg);
     return stream.str();
 }
 
-std::string Address::toString() const
+std::string Chip8_internals::Address::toString() const
 {
     std::stringstream stream;
     stream << "0x" << std::hex << address;
     return stream.str();
 }
 
-const Address Chip8::top_stack() const
+const Chip8_internals::Address Chip8::Chip8::top_stack() const
 {
     return stack[SP];
 }
 
-void Chip8::call(const uint16_t nnn)
+Chip8_internals::Pixel Chip8_internals::Pixel::operator^(uint8_t u) const
+{
+    if (int(status) ^ u)
+    {
+        return Chip8_internals::Pixel(Chip8_internals::Status::on);
+    }
+    else
+    {
+        return Chip8_internals::Pixel(Chip8_internals::Status::off);
+    }
+}
+
+bool Chip8_internals::Display::drw(auto a, const uint8_t x, const uint8_t y)
+{
+    bool res = false;
+    size_t s = a.size();
+    assert(x<64 && y<32 && s<16);
+    for (size_t i=0; i<s; ++i)
+    {
+        int k = i % 32;
+        for (int j=0; j<8; ++j)
+        {
+            int l = j % 64;
+            bool r = (d[k][l].status == Chip8_internals::Status::on);
+            d[k][l] = d[k][l] ^ static_cast<uint8_t>(a[i] & 1);
+            a[i] = a[i] >> 1u;
+            if (!res)
+            {
+                res = r && (d[k][l].status == Chip8_internals::Status::off);
+            }
+        }
+    }
+    return res;
+}
+
+void Chip8::Chip8::call(const uint16_t nnn)
 {
     ++SP;
     stack[SP] = PC;
-    PC = Address(nnn);
+    PC = Chip8_internals::Address(nnn);
 }
 
-void Chip8::se(const uint16_t xkk)
+void Chip8::Chip8::se(const uint16_t xkk)
 {
     uint8_t x = (xkk & 0xf00) >> 8u; // second 4 bits
     uint8_t kk = static_cast<uint8_t>(xkk & 0xff); // last 8 bits
@@ -36,7 +72,7 @@ void Chip8::se(const uint16_t xkk)
     }
 }
 
-void Chip8::sne(const uint16_t xkk)
+void Chip8::Chip8::sne(const uint16_t xkk)
 {
     uint8_t x = (xkk & 0xf00) >> 8u; // second 4 bits
     uint8_t kk = static_cast<uint8_t>(xkk & 0xff); // last 8 bits
@@ -46,7 +82,7 @@ void Chip8::sne(const uint16_t xkk)
     }
 }
 
-void Chip8::se(const uint8_t xy)
+void Chip8::Chip8::se(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
@@ -56,49 +92,49 @@ void Chip8::se(const uint8_t xy)
     }
 }
 
-void Chip8::ld(const uint16_t xkk)
+void Chip8::Chip8::ld(const uint16_t xkk)
 {
     uint8_t x = (xkk & 0xf00) >> 8u;
     uint8_t kk = static_cast<uint8_t>(xkk & 0xff);
     registers[x].update(kk);
 }
 
-void Chip8::add(const uint16_t xkk)
+void Chip8::Chip8::add(const uint16_t xkk)
 {
     uint8_t x = (xkk & 0xf00) >> 8u;
     uint16_t kk = static_cast<uint16_t>(xkk & 0xff);
     registers[x] += kk;
 }
 
-void Chip8::ld(const uint8_t xy)
+void Chip8::Chip8::ld(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
     registers[x].update(registers[y].reg);
 }
 
-void Chip8::bit_or(const uint8_t xy)
+void Chip8::Chip8::bit_or(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
     registers[x].update(registers[x].reg | registers[y].reg);
 }
 
-void Chip8::bit_and(const uint8_t xy)
+void Chip8::Chip8::bit_and(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
     registers[x].update(registers[x].reg & registers[y].reg);
 }
 
-void Chip8::bit_xor(const uint8_t xy)
+void Chip8::Chip8::bit_xor(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
     registers[x].update(registers[x].reg ^ registers[y].reg);
 }
 
-void Chip8::add(const uint8_t xy)
+void Chip8::Chip8::add(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
@@ -114,7 +150,7 @@ void Chip8::add(const uint8_t xy)
     }
 }
 
-void Chip8::sub(const uint8_t xy)
+void Chip8::Chip8::sub(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
@@ -131,7 +167,7 @@ void Chip8::sub(const uint8_t xy)
     registers[x].update(static_cast<uint8_t>(val_x - val_y));
 }
 
-void Chip8::shr(const uint8_t xy)
+void Chip8::Chip8::shr(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t val_x = registers[x].reg;
@@ -146,7 +182,7 @@ void Chip8::shr(const uint8_t xy)
     registers[x].reg /= 2;
 }
 
-void Chip8::subn(const uint8_t xy)
+void Chip8::Chip8::subn(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
@@ -163,7 +199,7 @@ void Chip8::subn(const uint8_t xy)
     registers[x].update(static_cast<uint8_t>(val_y - val_x));
 }
 
-void Chip8::shl(const uint8_t xy)
+void Chip8::Chip8::shl(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t val_x = registers[x].reg;
@@ -178,7 +214,7 @@ void Chip8::shl(const uint8_t xy)
     registers[x].update(static_cast<uint8_t>(val_x*2));
 }
 
-void Chip8::sne(const uint8_t xy)
+void Chip8::Chip8::sne(const uint8_t xy)
 {
     uint8_t x = (xy & 0xf0) >> 4u;
     uint8_t y = xy & 0xf;
@@ -188,17 +224,17 @@ void Chip8::sne(const uint8_t xy)
     }
 }
 
-void Chip8::ld_I(const uint16_t nnn)
+void Chip8::Chip8::ld_I(const uint16_t nnn)
 {
     I = nnn;
 }
 
-void Chip8::jp_v0(const uint16_t nnn)
+void Chip8::Chip8::jp_v0(const uint16_t nnn)
 {
     PC += registers[0] + nnn;
 }
 
-void Chip8::rnd(const uint16_t xkk)
+void Chip8::Chip8::rnd(const uint16_t xkk)
 {
     static std::mt19937 generator = std::mt19937();
     static std::uniform_int_distribution<> distrib(0, 255);
@@ -208,12 +244,31 @@ void Chip8::rnd(const uint16_t xkk)
     registers[x].update( rd & kk );
 }
 
-void Chip8::add_I(const uint8_t x)
+void Chip8::Chip8::drw(const uint16_t xyn)
+{
+    uint8_t x = (xyn & 0xf00) >> 8u;
+    uint8_t y = (xyn & 0xf0) >> 4u;
+    uint8_t n = static_cast<uint8_t>(xyn & 0xf);
+    uint8_t coord_x = (registers[x].reg) % 64;
+    uint8_t coord_y = (registers[y].reg) % 32;
+    auto a = std::ranges::take_view(std::ranges::drop_view(ram, I-1),n);
+    bool set = display.drw(a, coord_x, coord_y);
+    if (set)
+    {
+        registers[0xf].update(1);
+    }
+    else
+    {
+        registers[0xf].update(0);
+    }
+}
+
+void Chip8::Chip8::add_I(const uint8_t x)
 {
     I = registers[x] + I;
 }
 
-void Chip8::ld_B(const uint8_t x)
+void Chip8::Chip8::ld_B(const uint8_t x)
 {
     uint8_t val_x = registers[x].reg;
     ram[I] = static_cast<uint8_t>(val_x / 100);
@@ -223,7 +278,7 @@ void Chip8::ld_B(const uint8_t x)
     ram[I+2] = static_cast<uint8_t>(val_x);
 }
 
-void Chip8::ldIVx(const uint8_t x)
+void Chip8::Chip8::ldIVx(const uint8_t x)
 {
     uint16_t J = I;
     for (int i : std::ranges::iota_view(0, x+1))
@@ -233,7 +288,7 @@ void Chip8::ldIVx(const uint8_t x)
     }
 }
 
-void Chip8::ldVxI(const uint8_t x)
+void Chip8::Chip8::ldVxI(const uint8_t x)
 {
     uint16_t J = I;
     for (int i : std::ranges::iota_view(0, x+1))
@@ -243,11 +298,11 @@ void Chip8::ldVxI(const uint8_t x)
     }
 }
 
-std::vector<Instruction> Chip8::readFromFile(const std::filesystem::path path) const
+std::vector<Chip8_internals::Instruction> Chip8::Chip8::readFromFile(const std::filesystem::path path) const
     {
         assert(exists(path));
 
-        std::vector<Instruction> output;
+        std::vector<Chip8_internals::Instruction> output;
         std::ifstream file {path};
 
         if (file.is_open())
@@ -272,21 +327,25 @@ std::vector<Instruction> Chip8::readFromFile(const std::filesystem::path path) c
         }
     }
 
-void Chip8::run(const std::vector<Instruction> instructions)
+void Chip8::Chip8::run(const std::vector<Chip8_internals::Instruction> instructions)
     {
-        for (Instruction instruction : instructions)
+        for (Chip8_internals::Instruction instruction : instructions)
         {
             execute(instruction);
         }
     }
 
-void Chip8::execute(const Instruction i)
+void Chip8::Chip8::execute(const Chip8_internals::Instruction i)
 {
     uint16_t inst = i.instruction();
     switch (inst)
     {
     case static_cast<uint16_t>(0x00ee):
         ret();
+        break;
+
+    case static_cast<uint16_t>(0x00e0):
+        cls();
         break;
 
     default:
@@ -455,6 +514,13 @@ void Chip8::execute(const Instruction i)
     {
         uint16_t xkk = inst & 0xfff;
         rnd(xkk);
+        break;
+    }
+
+    case 0xd:
+    {
+        uint16_t xyn = inst & 0xfff;
+        drw(xyn);
         break;
     }
 
--- src/chip8_emulator/chip8.h
+++ src/chip8_emulator/chip8.h
@@ -10,171 +10,233 @@
 #include <random>
 #include <ranges>
 
+namespace Chip8{
+class Chip8;
+}
+
+namespace Chip8_internals {
+    class Register {
+    public:
+        Register() : reg{ 0 } {}
+        Register(uint8_t u) : reg{ u } {}
+
+        bool operator==(const uint8_t u) const { return reg ==u; }
+
+        bool operator==(const Register r) const { return reg == r.reg; }
+
+        bool operator!=(const uint8_t u) const { return reg != u; }
+
+        void update(const uint8_t u) { reg = u; }
+
+        void operator+=(const uint16_t u) { reg = static_cast<uint8_t>(reg + u); }
+
+        uint16_t operator+(const uint16_t u) { return static_cast<uint16_t>(u + reg); }
+
+        std::string toString() const;
+
+        friend class Chip8::Chip8;
+
+    private:
+        uint8_t reg; // the usual register has 8 bits
+    };
+
+    class Address {
+    public:
+        Address() : address{ 0 } {}
+        Address(const uint16_t u) : address{ u } {}
+
+        bool isEmpty() const { return address == 0; }
+
+        std::string toString() const;
+
+        bool operator==(Address a) const { return address == a.address; }
+
+        void operator+=(int i) { address = static_cast<uint16_t>(address+i); }
+
+    private:
+        uint16_t address; // an address consists of 12 bits, so only the rightmost 12 bits are gonna be filled
+    };
+
+    class Instruction {
+    public:
+        explicit Instruction(const uint16_t i) : inst{ i } {}
+
+        void operator+=(const uint16_t x) { inst = static_cast<uint16_t>(inst + x); }
+
+        uint16_t instruction() const {return inst;}
+
+    private:
+        uint16_t inst;
+
+    };
+
+    enum class Status {off, on};
+
+    class Pixel {
+    public:
+        Pixel() : status{ Status::off } { }
+        Pixel(Status s) : status{ s } { }
+        Pixel operator^(uint8_t u) const;
+
+    //private:
+        Status status;
+    };
+
+    class Display {
+    public:
+        Display() : d{ std::array<std::array<Pixel, 64>, 32>() } {}
+
+        // takes the vector v and does xor with the pixels at starting at coordinate (x,y)
+        // returns true if this causes any pixel to be unset and false otherwise
+        bool drw(auto a, const uint8_t x, const uint8_t y);
+
+    private:
+        std::array<std::array<Pixel, 64>, 32> d;
+    };
+
+    /*template<uint8_t n>
+        requires (n > 0) && (n < 16)
+    class Sprite {
+    public:
+        Sprite() : sprite { std::array<std::array<Pixel, 8>, n>() } {}
+        Sprite(std::array< uint8_t, n> a) : sprite { std::array<std::array<Pixel, 8>, n>() }
+        {
+            for (int i : std::ranges::iota_view{0,n})
+            {
+                uint8_t u = a[i];
+                for (int j=0; j<8; ++j)
+                {
+                    if (u & 1 == 1)
+                    {
+                        sprite[i][j] = Pixel(Status::on);
+                    }
+                    u >> 1u;
+                }
+            }
+        }
+    private:
+        std::array<std::array<Pixel, 8>, n> sprite;
+    };*/
+}
+
+namespace Chip8 {
+    class Chip8 {
+    public:
+        Chip8() :
+        ram{ std::array<uint8_t, 4096>() },
+        registers{ std::array<Chip8_internals::Register, 16>() },
+        I{ 0 },
+        timer{ Chip8_internals::Register() },
+        sound{ Chip8_internals::Register() },
+        PC{ Chip8_internals::Address() },
+        SP{ 0 },
+        stack{ std::array<Chip8_internals::Address, 16>() },
+        display{ Chip8_internals::Display() }  {}
+
+        // read instructions from file and return a vector with instructions in hexadecimal ints
+        std::vector<Chip8_internals::Instruction> readFromFile(const std::filesystem::path path) const;
+
+        // runs the instructions
+        void run(const std::vector<Chip8_internals::Instruction> instructions);
+
+        // instruction 00e0
+        void cls() { display = Chip8_internals::Display(); }
+
+        // instruction 00ee
+        void ret() { PC = top_stack(); --SP; }
+
+        // instrucion 1nnn
+        void jp(const uint16_t nnn) { PC = nnn; }
+
+        // instruction 2nnn
+        void call(const uint16_t nnn);
+
+        // instruction 3xkk
+        void se(const uint16_t xkk);
+
+        // instruction 4xkk
+        void sne(const uint16_t xkk);
+
+        // instruction 5xy0
+        void se(const uint8_t xy);
+
+        // instruction 6xkk
+        void ld(const uint16_t xkk);
+
+        // instruction 7xkk
+        void add(const uint16_t xkk);
+
+        // instruction 8xy0
+        void ld(const uint8_t xy);
+
+        // instruction 8xy1
+        void bit_or(const uint8_t xy);
+
+        // instruction 8xy2
+        void bit_and(const uint8_t xy);
+
+        // instruction 8xy3
+        void bit_xor(const uint8_t xy);
+
+        // instruction 8xy4
+        void add(const uint8_t xy);
+
+        // instruction 8xy5
+        void sub(const uint8_t xy);
+
+        // instruction 8xy6
+        void shr(const uint8_t xy);
+
+        // instruction 8xy7
+        void subn(const uint8_t xy);
+
+        // instruction 8xye
+        void shl(const uint8_t xy);
+
+        // instruction 9xy0
+        void sne(const uint8_t xy);
+
+        // instruction annn
+        void ld_I(const uint16_t nnn);
+
+        // instruction bnnn
+        void jp_v0(const uint16_t nnn);
+
+        // instruction cxkk
+        void rnd(const uint16_t xkk);
+
+        // instruction dxyn
+        void drw(const uint16_t xyn);
+
+        // instruction fx1e
+        void add_I(const uint8_t x);
+
+        // instruction fx33
+        void ld_B(const uint8_t x);
+
+        // instruction fx55
+        void ldIVx(const uint8_t x);
+
+        // instruction fx65
+        void ldVxI(const uint8_t x);
+
+        void execute(const Chip8_internals::Instruction i);
 
 
-class Register{
-public:
-    Register() : reg{ 0 } {}
-    Register(uint8_t u) : reg{ u } {}
+    //private:
+        // the ram consists of register 0 to 4095 and each register has 8 bits
+        std::array<uint8_t, 4096> ram;
 
-    bool operator==(const uint8_t u) const { return reg ==u; }
+        std::array<Chip8_internals::Register, 16> registers; // chip-8 has 16 registers of 8 bits
+        uint16_t I; // 16-bits register to store memory address
+        Chip8_internals::Register timer; // 8-bits register for delay
+        Chip8_internals::Register sound; // 8-bits register for sound
+        Chip8_internals::Address PC; // program counter
+        uint8_t SP; // 8 bits for pointing to the topmost level of the stack
+        std::array<Chip8_internals::Address, 16> stack; // the stack is an array of 16 16-bits values to store addresses
+        Chip8_internals::Display display;
 
-    bool operator==(const Register r) const { return reg == r.reg; }
+        const Chip8_internals::Address top_stack() const;
 
-    bool operator!=(const uint8_t u) const { return reg != u; }
-
-    void update(const uint8_t u) { reg = u; }
-
-    void operator+=(const uint16_t u) { reg = static_cast<uint8_t>(reg + u); }
-
-    uint16_t operator+(const uint16_t u) { return static_cast<uint16_t>(u + reg); }
-
-    std::string toString() const;
-
-    friend class Chip8;
-
-private:
-    uint8_t reg; // the usual register has 8 bits
-};
-
-class Address{
-public:
-    Address() : address{ 0 } {}
-    Address(const uint16_t u) : address{ u } {}
-
-    bool isEmpty() const { return address == 0; }
-
-    std::string toString() const;
-
-    bool operator==(Address a) const { return address == a.address; }
-
-    void operator+=(int i) { address = static_cast<uint16_t>(address+i); }
-
-private:
-    uint16_t address; // an address consists of 12 bits, so only the rightmost 12 bits are gonna be filled
-};
-
-class Instruction{
-public:
-    explicit Instruction(const uint16_t i) : inst{ i } {}
-
-    void operator+=(const uint16_t x) { inst = static_cast<uint16_t>(inst + x); }
-
-    uint16_t instruction() const {return inst;}
-
-private:
-    uint16_t inst;
-
-};
-
-class Chip8{
-public:
-    Chip8() :
-    ram{ std::array<uint8_t, 4096>() },
-    registers{ std::array<Register, 16>() },
-    I{ 0 },
-    timer{ Register() },
-    sound{ Register() },
-    PC{ Address() },
-    SP{ 0 },
-    stack{ std::array<Address, 16>() }  {}
-
-    // read instructions from file and return a vector with instructions in hexadecimal ints
-    std::vector<Instruction> readFromFile(const std::filesystem::path path) const;
-
-    // runs the instructions
-    void run(const std::vector<Instruction> instructions);
-
-    // instruction 00ee
-    void ret() { PC = top_stack(); --SP; }
-
-    // instrucion 1nnn
-    void jp(const uint16_t nnn) { PC = nnn; }
-
-    // instruction 2nnn
-    void call(const uint16_t nnn);
-
-    // instruction 3xkk
-    void se(const uint16_t xkk);
-
-    // instruction 4xkk
-    void sne(const uint16_t xkk);
-
-    // instruction 5xy0
-    void se(const uint8_t xy);
-
-    // instruction 6xkk
-    void ld(const uint16_t xkk);
-
-    // instruction 7xkk
-    void add(const uint16_t xkk);
-
-    // instruction 8xy0
-    void ld(const uint8_t xy);
-
-    // instruction 8xy1
-    void bit_or(const uint8_t xy);
-
-    // instruction 8xy2
-    void bit_and(const uint8_t xy);
-
-    // instruction 8xy3
-    void bit_xor(const uint8_t xy);
-
-    // instruction 8xy4
-    void add(const uint8_t xy);
-
-    // instruction 8xy5
-    void sub(const uint8_t xy);
-
-    // instruction 8xy6
-    void shr(const uint8_t xy);
-
-    // instruction 8xy7
-    void subn(const uint8_t xy);
-
-    // instruction 8xye
-    void shl(const uint8_t xy);
-
-    // instruction 9xy0
-    void sne(const uint8_t xy);
-
-    // instruction annn
-    void ld_I(const uint16_t nnn);
-
-    // instruction bnnn
-    void jp_v0(const uint16_t nnn);
-
-    // instruction cxkk
-    void rnd(const uint16_t xkk);
-
-    // instruction fx1e
-    void add_I(const uint8_t x);
-
-    // instruction fx33
-    void ld_B(const uint8_t x);
-
-    // instruction fx55
-    void ldIVx(const uint8_t x);
-
-    // instruction fx65
-    void ldVxI(const uint8_t x);
-
-    void execute(const Instruction i);
-
-
-//private:
-    // the ram consists of register 0 to 4095 and each register has 8 bits
-    std::array<uint8_t, 4096> ram;
-
-    std::array<Register, 16> registers; // chip-8 has 16 registers of 8 bits
-    uint16_t I; // 16-bits register to store memory address
-    Register timer; // 8-bits register for delay
-    Register sound; // 8-bits register for sound
-    Address PC; // program counter
-    uint8_t SP; // 8 bits for pointing to the topmost level of the stack
-    std::array<Address, 16> stack; // the stack is an array of 16 16-bits values to store addresses
-
-    const Address top_stack() const;
-
-};
+    };
+}
