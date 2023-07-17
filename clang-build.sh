df9e27807a3c0c4a36283035dfcded02a06cdc83
Martina Fruttidoro <fruttidoro95@gmail.com>
2023-07-17 12:28:16 +0200

    created two build files for linux with gcc and clang

---
+++ clang-build.sh
@@ -0,0 +1,26 @@
+clang++ --std=c++20 \
+-ggdb \
+-fdiagnostics-show-template-tree \
+-Wextra \
+-Wall \
+-Wpedantic \
+-Wconversion \
+-Wwrite-strings \
+-Warray-bounds \
+-Wcast-align \
+-Wcast-qual \
+-Wdouble-promotion \
+-Wnull-dereference \
+-Wimplicit-fallthrough \
+-Wshift-negative-value \
+-Wswitch-default \
+-Wswitch-enum \
+-Wuninitialized \
+-Wfloat-equal \
+-Wshadow \
+-Wvla \
+-Wno-unknown-pragmas \
+./src/chip8_emulator/chip8.cpp \
+./tests/test.cpp \
+-o bin/chip8.bin
+
